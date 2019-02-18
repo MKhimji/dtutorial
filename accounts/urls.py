7e21d9ff3aefe951d66f762185a02c3c781d976b
MKhimji <muhammadkhimji@live.co.uk>
2019-02-18 03:58:37 +0000

    email settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,7 @@ from .import views
 from django.contrib.auth.views import login, logout, password_reset, password_reset_done, password_reset_confirm,password_reset_complete
 from tutorial import settings
 
-##avoid hardcoding urls in project
-##so use 'name=' in the url pattern and reverse function so that if you change
-##a url from say account/login to account/users/login then you wuldnt have to change
-##that url everywhere in your project rather you take advantage of the name= and reverse function
+
 
 urlpatterns = [
 
@@ -25,16 +22,12 @@ urlpatterns = [
 
     url(r'^reset-password/done/$', password_reset_done, {'template_name': 'accounts/reset_password_done.html'}, name = 'password_reset_done'),
 
-    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-    password_reset_confirm,{'post_reset_redirect':
-    'accounts:password_reset_complete'},name ='password_reset_confirm'),
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name': 'accounts/reset_password_confirm.html', 'post_reset_redirect': 'accounts:password_reset_complete'}, name='password_reset_confirm'),
 
-    url(r'^reset-password/complete/$',password_reset_complete, name = 'password_reset_complete'),
+
+    url(r'^reset-password/complete/$', password_reset_complete,{'template_name': 'accounts/reset_password_complete.html'}, name='password_reset_complete'),
     
-    url(r'^ajax/validate_username/$', views.validate_username, name='validate_username'),
+    url(r'^ajax/validate_username/$', views.validate_username, name='validate_username')
 ]
 
-    ##url(r'^blog/(?P<pk>\d+)/$', views.view_blogpost,name = 'view_blogpost_with_pk')
 
-
-## url(r'^blog/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.view_blogpost,name = 'view_blogpost')
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -121,8 +121,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'tutorial/media')
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
-
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 CKEDITOR_UPLOAD_PATH = 'Screenshots/'
 CKEDITOR_IMAGE_BACKEND = 'pillow'
@@ -148,7 +147,7 @@ LOGIN_EXEMPT_URLS = (
     r'^account/register/$',
     r'^account/reset-password/$',
     r'^account/reset-password/done/$',
-    r'^account/reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+    r'^account/reset-password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
     r'^account/reset-password/complete/$'
 
 )
